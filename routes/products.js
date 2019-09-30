f1252dc73bd76885ae2e02caa8b85f69fc1a77e8
scottefloyd <scottefloyd@gmail.com>
2019-09-29 22:02:20 -0400

    finished tutorial

--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
 const checkAuth = require('../middleware/check-auth');
 const multer = require('multer');
+const productsController = require('../controllers/products');
 
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
@@ -33,142 +33,14 @@ const upload = multer({
 
 const Product = require('../models/product');
 
-router.get('/', (req, res, next) => {
-    Product.find()
-    .select('name price _id productImage')
-    .exec()
-    .then(docs => { 
-       const response = {
-           count: docs.length,
-           products: docs.map(doc => {
-               return {
-                   name: doc.name,
-                   price: doc.price,
-                   _id: doc._id,
-                   request: {
-                       type: 'GET',
-                       url: 'http://localhost:3000/products/' + doc._id
-                   }
-               }
-           })
-       }
-        res.status(200).json(response);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error:err
-        });
-        
-    });
-});
+router.get('/', productsController.get_products);
  
-router.post('/', upload.single('productImage'),  (req, res, next) => {
-    console.log(req.file);
-    
-    const product = new Product({
-        _id: new mongoose.Types.ObjectId(),
-        name: req.body.name,
-        price: req.body.price,
-        productImage: req.file.path
-    });
-    product.save()
-    .then(result => {
-        console.log(result);
-        res.status(201).json({
-            message: 'Created product successfully',
-            createdProduct: {
-                name: result.price,
-                price: result.price,
-                _id: result._id,
-                request: {
-                    type: 'GET',
-                    url: "http://localhost:3000/products/" + result._id
-                }
-            }
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
-    });
-});
+router.post('/', checkAuth, upload.single('productImage'), productsController.post_product);
 
-router.get('/:productId', (req, res, next) => {
-    const id = req.params.productId;
-    Product.findById(id)
-    .select('name price _id productImage')
-    .exec()
-    .then(doc => {
-        if (doc) {
-            res.status(200).json({
-                message: 'check out this awesome product',
-                Product: doc,
-                request: {
-                    type: 'GET',
-                    url: "http://localhost:3000/products/" + doc._id
-                }
-            });
-        } else {
-            res.status(404).json({message: "no valide entry found for ID"});
-        }
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({error: err});
-    });
-});
+router.get('/:productId', productsController.get_single_product);
 
-router.patch('/:productId', (req, res, next) => {
-     const id = req.params.productId;
-     const updateOps = {};
-     for (const ops of req.body) {
-         updateOps[ops.propName] = ops.value;
-         console.log(ops.value);
-         
-     }
-     Product.update({_id: id}, { $set: updateOps})
-        .exec()
-        .then(result => {
-            res.status(200).json({
-                message: 'Product updated',
-                request: {
-                    type: 'GET',
-                    url: 'http://localhost:3000/products/' + id
-                }
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                error: err
-            });
-        });
-    
-});
+router.patch('/:productId', checkAuth, productsController.patch_product);
 
-router.delete('/:productId', (req, res, next) => {
-    const id = req.params.productId;
-    Product.remove({_id: id})
-    .exec()
-    .then(result => {
-        res.status(200).json({
-            message: 'product deleted',
-            request: {
-                type: 'POST',
-                url: 'http://localhost:3000/products',
-                data: {name: 'String', price: 'Number'}
-            }
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
-    });
-});
+router.delete('/:productId', checkAuth, productsController.delete_product);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,168 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const checkAuth = require('../middleware/check-auth');
 
-const User = require('../models/user');
+const userController = require('../controllers/users');
 
-router.post("/login", (req, res, next) => {
-    User.find({ email: req.body.email })
-      .exec()
-      .then(user => {
-        if (user.length < 1) {
-          return res.status(401).json({
-            message: "Auth failed"
-          });
-        }
-        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-          if (err) {
-            return res.status(401).json({
-              message: "Auth failed"
-            });
-          }
-          if (result) {
-            const token = jwt.sign(
-              {
-                email: user[0].email,
-                userId: user[0]._id
-              },
-              process.env.JWT_KEY,
-              
-              {
-                  expiresIn: "1h"
-              }
-            );
-            return res.status(200).json({
-              message: "Auth successful",
-              token: token
-            });
-          }
-          res.status(401).json({
-            message: "Auth failed"
-          });
-        });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({
-          error: err
-        });
-      });
-  });
+router.post("/login", userController.user_login);
 
-router.get("/", (req, res, next) => {
-    User.find()
-        .select('email')
-        .then(docs => {
-            const response = {
-                count: docs.length,
-                users: docs.map(doc => {
-                    return {
-                        email: doc.email,
-                        _id: doc._id,
-                        request: {
-                            type: 'GET',
-                            url: 'http://localhost:3000/users/' + doc._id
-                        }
-                    }
-                })
-            }
-            res.status(200).json(response);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                error: err
-            });
+router.get("/", userController.get_all_users);
 
-        });
-});
+router.get("/:userId", userController.get_single_user);
 
-router.get("/:userId", (req, res, next) => {
-    const id = req.params.userId;
-    User.findById(id)
-        .select('email')
-        .exec()
-        .then(doc => {
-            if (doc) {
-                res.status(200).json({
-                    message: 'check out this awesome user',
-                    user: doc,
-                    request: {
-                        type: 'GET',
-                        url: "http://localhost:3000/users/" + doc._id
-                    }
-                });
-            } else {
-                res.status(404).json({ message: "no valid entry found for ID" });
-            }
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({ error: err });
-        });
-});
+router.post("/signup", userController.signup_user);
 
-router.post("/signup", (req, res, next) => {
-    User.find({ email: req.body.eamil })
-        .exec()
-        .then(user => {
-            if (user.length >= 1) {
-                return res.status(409).json({
-                    message: 'mail already exists'
-                })
-            } else {
-                bcrypt.hash(req.body.password, 10, (err, hash) => {
-                    if (err) {
-                        return res.status(500).json({
-                            error: err
-                        });
-                    } else {
-                        const user = new User({
-                            _id: new mongoose.Types.ObjectId(),
-                            email: req.body.email,
-                            password: hash
-                        });
-                        user.save()
-                            .then(result => {
-                                console.log(result);
-                                res.status(201).json({
-                                    message: 'user created'
-                                });
-                            })
-                            .catch(err => {
-                                console.log(err);
-                                res.status(500).json({
-                                    error: err
-                                });
-
-                            })
-                    }
-                });
-            }
-        });
-});
-
-router.delete('/:userId', (req, res, next) => {
-    User.remove({ _id: req.params.id })
-        .exec()
-        .then(result => {
-            res.status(200).json({
-                message: 'user deleted',
-                request: {
-                    type: 'delete',
-                    url: 'http://localhost:3000/users',
-                    data: { email: 'String' }
-                }
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                error: err
-            });
-        });
-});
+router.delete("/:userId", checkAuth, userController.delete_user);
 
 module.exports = router;
